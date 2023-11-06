d19ca462585b5f5192a62aaaa86df06d096e934c
abdo754 <W0786364@myscc.ca>
2023-11-06 12:51:03 -0500

    change some things

--- AbdulrhmanBooks.DataAccess/Data/ApplicationDbContext.cs
+++ AbdulrhmanBooks.DataAccess/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace AbdulrhmanBooksStore.Data
+namespace AbdulrhmanBooksStore.DataAccess.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
